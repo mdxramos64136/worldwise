ffd681489597381e7797b86b4e951bcb6147cff0
Marcel Ramos <marceldavid@hotmail.com>
2025-07-05 12:27:43 -0400

    build AppLayout component  and set nested Routes

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,16 @@
 import { nav } from "./AppNav.module.css";
-
+import { NavLink } from "react-router-dom";
 function AppNav() {
-  return <div className={nav}>App Navigation</div>;
+  return (
+    <div className={nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default AppNav;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,14 @@
 /**Main Aplication: List on the leftside and map on the right side */
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
